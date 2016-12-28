99471205cf5df30900bd6d63e13d134d550e85d6
amanxsharma <aman.sharma728@gmail.com>
2016-12-28 13:56:00 -0500

    Conditional Statements
    Conditional Statements
    if-elif

---
+++ if-elif.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import sys
+
+
+N = int(raw_input().strip())
+if N%2 !=0:
+    print "Weird"
+elif N%2==0 and N in range(2,5):
+    print "Not Weird"
+elif N%2==0 and N>=6 and N<=20:
+    print "Weird"
+elif N%2==0 and N>20:
+    print "Not Weird"
